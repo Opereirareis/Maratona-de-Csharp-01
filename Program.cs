f323a3d4df5e47a63661c5104e17ce1078d0528c
Ernan <90629225+Opereirareis@users.noreply.github.com>
2025-11-10 17:28:18 -0300

    Implement Celsius to Fahrenheit conversion
    Added temperature conversion from Celsius to Fahrenheit.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+//01 - 
+
 using System;
 
 class Program
@@ -14,5 +16,27 @@ class Program
         // 3. Usando Interpolação ($)
         string saudacao2 = $"Olá, {nome}! Bem-vindo ao nosso site!";
         Console.WriteLine(saudacao2);
+    }
+}
+// 02 - 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Variáveis e constantes
+        double celsius = 20.0;
+        
+        // Para a divisão 9/5 funcionar certo, usamos "9.0" ou "5.0" (double)
+        // Se usarmos "9/5" (int), o C# acha que o resultado é 1 (inteiro).
+        const double fatorConversao = 9.0 / 5.0; 
+        const double ajuste = 32;
+
+        // Cálculo
+        double fahrenheit = (celsius * fatorConversao) + ajuste;
+
+        // Exibição
+        Console.WriteLine($"{celsius}°C é igual a {fahrenheit}°F.");
     }
 }
